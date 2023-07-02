2a8740de1e66011bc5b2f140d0a1b01e37bf3c77
Vlad Kupchik <vladkupchik15@gmail.com>
2023-07-02 23:56:30 +0300

    variables-1

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -4,8 +4,10 @@ public class ReassigningValues {
     public static void main(String[] args) {
         int first = 1, second = 10, third = 100;
         System.out.println(first + "\n" + second + "\n" + third);
-        int linkToFirst = first = 15, linkToSecond = second = 6, lintToThird = third = 4;
+        first = 15;
+        second = 6;
+        third = 4;
+        int linkToFirst = first, linkToSecond = second, lintToThird = third;
         System.out.println(linkToFirst + "\n" + linkToSecond + "\n" + lintToThird);
-        System.out.println(first + "\n" + second + "\n" + third);
     }
 }
